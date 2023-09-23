66e5dd1197e5bcd8fa4279b2bfb58355ddff9e19
Jude Abishek Satish <jude-abishek-satish@hpe.com>
2023-09-24 03:11:49 +0530

    Code changes

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-import requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -7,30 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_location():
-    # Get the user's IP address from the request object
-    user_ip = request.remote_addr
-    response = requests.get(f'https://ipinfo.io/{user_ip}/json', verify=False)
-    user_ip_results = response.json()
-    # latitude, longitude = user_ip_results.get('loc').split(',')
-
-    weather_url = "https://atlas.microsoft.com/weather/currentConditions/json?" \
-                  f"api-version=1.0&query=47.60357,-122.32945&" \
-                  "subscription-key=VCjx6AU_MxmuLPOfiXlqcF-M541X48W0IyvzbvMRGCQ"
-
-    location_url = "https://atlas.microsoft.com/search/address/reverse/json?" \
-                   f"api-version=1.0&query=47.60357,-122.32945&" \
-                   "subscription-key=VCjx6AU_MxmuLPOfiXlqcF-M541X48W0IyvzbvMRGCQ"
-
-    weather_response = requests.get(weather_url, verify=False)
-    location_response = requests.get(location_url, verify=False)
-    weather_result = weather_response.json()
-    location_result = location_response.json()
 
     data = {
-        "weather": weather_result["results"][0]["phrase"],
-        "city": location_result["addresses"][0]["address"]["municipality"],
-        "country": location_result["addresses"][0]["address"]["country"],
-        "temperature": weather_result["results"][0]["realFeelTemperature"]["value"],
+        "weather": "Rain",
+        "city": "Bangalore",
+        "country": "India",
+        "temperature": "21",
     }
 
     return render_template('index.html', data=data)
