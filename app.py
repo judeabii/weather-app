5ad00c082be218d7cbbbec888e1830138f8b2d5a
Jude Abishek Satish <jude-abishek-satish@hpe.com>
2023-09-24 11:48:22 +0530

    Code changes

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request
-
+import requests
 app = Flask(__name__)
 
 
 @app.route('/')
 def get_location():
+    weather_url = "https://atlas.microsoft.com/weather/currentConditions/json?" \
+                  f"api-version=1.0&query=47.60357,-122.32945&" \
+                  "subscription-key=VCjx6AU_MxmuLPOfiXlqcF-M541X48W0IyvzbvMRGCQ"
+    weather_response = requests.get(weather_url, verify=False)
+    weather_result = weather_response.json()
 
     data = {
         "weather": "Rain",
